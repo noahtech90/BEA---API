fd43427b1dd24afa79bd6eccb0e251d5fc52e32f
Noah Hodge <noahhodge2@gmail.com>
2021-12-03 19:02:11 -0600

    api construction function

--- common.py
+++ common.py
@@ -1,9 +1,42 @@
-def parse_comma_string_to_int(string):
-    '''
-    used to convert comma seperated dollar to integer
-    '''
-    try:    
-        number = int(string.replace(",", ""))
-    except: number = 0
-    return number
+import pandas as pd
+from pprint import pprint
+from urllib.request import urlopen
+import plotly.graph_objects as go
+import json
+import plotly.express as px
 
+
+def generate_df(**kwargs):
+
+    pass
+
+def income_per_capital(year, df):
+    '''
+    County view of income per capita in the US
+
+    data available starting around 1990???
+    '''
+    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+        counties = json.load(response)
+
+    def clean_data(df):
+        df['DataValue'] = df['DataValue'].apply(lambda x: x.replace(",",""))
+        df['DataValue'] = df['DataValue'].apply(lambda x: x.replace("(NA)", "0"))
+        df['DataValue'] = df['DataValue'].astype('int')
+        df['DataValue'] = df['DataValue'].apply(lambda x: x * (1.0232)**(2020 - int(year_setting))) 
+        return df
+
+    df = clean_data(df)
+
+    fig = px.choropleth(df, geojson=counties, locations='GeoFips', color='DataValue',
+                            color_continuous_scale="Viridis",
+                            range_color=(0, 110000),
+                            scope="usa",
+                            labels={'inc':'income per capital'},
+                            title='US Income Per Capita By County'
+                            )
+    fig.update_layout(
+        geo_scope='usa', # limite map scope to USA
+        margin={"r":0,"t":0,"l":0,"b":0}
+    )
+    fig.show()
