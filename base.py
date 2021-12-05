9a1aa78f65a517abb34697108ad410d3b275d54e
Noah Hodge <noahhodge2@gmail.com>
2021-12-05 17:52:37 -0600

    NIPA database class

--- base.py
+++ base.py
@@ -5,6 +5,7 @@ class BEA:
     def __init__(self):
         self.url = BASE_URL
         self.meta = self.Meta(self.url)
+        self.nipa = self.NIPA(self.url)
 
     class Meta():
         def __init__(self, url):
@@ -25,6 +26,35 @@ class BEA:
         
         def get_parameter_values(self, dataset, parameter_name):
             endpoint = self.url + f'&METHOD={METHOD["parameter_values"]}' + f'&DATASETNAME={dataset}'+ f'&ParameterName={parameter_name}'
+            response = requests.get(endpoint)
+            resp = response.json()
+            return resp
+        
+    class NIPA:
+        '''
+        GDP, Income, and Saving tables
+
+        Gross domestic product (GDP)
+        Gross domestic income (GDI)
+        National income
+        Corporate profits
+        Government receipts and expenditures
+        Personal income and disposable personal income
+        Personal consumption expenditures (PCE), or consumer spending
+        Personal saving
+
+        T50203 - saving and investment by sector
+        '''
+        def __init__(self, url):
+            self.url = url
+            super().__init__()
+
+        def access_table_data(self, table_id, freq, year):
+            endpoint = (self.url
+            + f'&METHOD={METHOD["get_data"]}'
+            + f'&DATASETNAME={"NIPA"}'+ f'&TableName={table_id}'
+            + f'&year={year}'
+            + f'&frequency={freq}')
             response = requests.get(endpoint)
             resp = response.json()
             return resp
