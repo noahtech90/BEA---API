6ba229302e8590eaae8c6b5c5792005d1f264893
Noah Hodge <noahhodge2@gmail.com>
2021-12-06 17:13:09 -0600

    show dataset tables

--- base.py
+++ base.py
@@ -1,13 +1,26 @@
 from settings import BASE_URL, METHOD, data_set_name, api_key
 import requests
 
+
 class BEA:
     def __init__(self):
         self.url = BASE_URL
+        self.dataset = None
         self.meta = self.Meta(self.url)
         self.nipa = self.NIPA(self.url)
+    
+    def show_dataset_tables(self):
+        if self.dataset is None:
+            return 'Method Not Allowed with this Dataset'
+        endpoint = self.url + f'&METHOD={METHOD["parameter_values"]}' + f'&DATASETNAME={self.dataset}'+ f'&ParameterName={"TableID"}'
+        response = requests.get(endpoint)
+        resp = response.json()
+        return resp
 
     class Meta():
+        '''
+        Used for obtaining meta data on all BEA Data tables
+        '''
         def __init__(self, url):
             self.url = url
             super().__init__()
@@ -47,15 +60,18 @@ class BEA:
         '''
         def __init__(self, url):
             self.url = url
+            self.dataset = 'NIPA'
             super().__init__()
 
         def access_table_data(self, table_id, freq, year):
             endpoint = (self.url
             + f'&METHOD={METHOD["get_data"]}'
-            + f'&DATASETNAME={"NIPA"}'+ f'&TableName={table_id}'
+            + f'&DATASETNAME={self.dataset}'+ f'&TableName={table_id}'
             + f'&year={year}'
             + f'&frequency={freq}')
             response = requests.get(endpoint)
             resp = response.json()['BEAAPI']['Results']['Data']
             return resp
+        
+
         
